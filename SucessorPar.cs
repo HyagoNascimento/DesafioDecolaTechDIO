28b9da5e029a19c6548dd89ed95080947607aada
Hyago Nascimento <79670510+HyagoNascimento@users.noreply.github.com>
2022-01-06 20:45:57 -0300

    Create SucessorPar.cs

---
+++ SucessorPar.cs
@@ -0,0 +1,18 @@
+using System; 
+
+class DIO {
+
+    static void Main(string[] args)
+    {
+    int x = int.Parse(Console.ReadLine());
+
+    if ((x + 1) % 2 == 0)//escreva aqui o seu código
+    {
+    Console.WriteLine(x + 1);
+      }
+      else
+      {
+      Console.WriteLine(x + 2);
+      }
+    }
+  }
