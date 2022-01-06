3ecf9e14b1e283ef306919459fe42c2e3e2777c9
Hyago Nascimento <79670510+HyagoNascimento@users.noreply.github.com>
2022-01-06 20:44:46 -0300

    Create SequenciaIJ.cs

---
+++ SequenciaIJ.cs
@@ -0,0 +1,19 @@
+using System; 
+
+class DIO {
+
+  static void Main(string[] args)
+    {
+  int a, b, c = 7;
+
+   for (int i = 1; i <= 9; i++)
+    {c = 7;
+      for (int j = 1; j <= 3; j++)    //escreva seu código nos espaços em branco
+       {if (i % 2 != 0)
+        {Console.WriteLine($"I={i} J={c}");
+        c--;
+              }
+            }
+          }
+      }
+    }
